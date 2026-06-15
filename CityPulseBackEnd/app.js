353e22d4b8062867618c583c72b8d98b2136491d
MKFaroouq <iprobazooka@gmail.com>
2026-06-15 17:12:30 +0300

    Env Ready !

--- CityPulseBackEnd/app.js
+++ CityPulseBackEnd/app.js
@@ -12,6 +12,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+
 // Port and database
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/citypulse';
