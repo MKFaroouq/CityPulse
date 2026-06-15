0823fb7c1145a3c3065a2865f4c71bc0a11158ae
MKFaroouq <iprobazooka@gmail.com>
2026-06-15 17:14:47 +0300

    Routes All Way Long 📍

---
+++ CityPulseBackEnd/routes/authRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {registerUser} = require("../controllers/authController");
+const {loginUser} = require("../controllers/authController");
+// const { registerUser, loginUser } = require("../controllers/authController");
+
+// more efficient way to import both functions from the same file.
+//1- const { registerUser, loginUser } = require("../controllers/authController");
+// importing all functions from the same file and then using them as authController.
+//2- const authController = require("../controllers/authController");
+
+
+// Register route
+router.post("/register", registerUser);
+
+// Login route
+router.post("/login", loginUser);
+
+module.exports = router;
